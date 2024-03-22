519a5ca80def28c6d5e068c4e404c4baec16da20
edouardbruelhart <edouard.bruelhart@unifr.ch>
2024-03-22 11:23:03 +0100

    iNaturalist import is now working

---
+++ inat_fetcher/src/db_updater.py
@@ -0,0 +1,1 @@
+print("correctly launched dbupdater")
--- inat_fetcher/src/fetcher.py
+++ inat_fetcher/src/fetcher.py
@@ -1,30 +1,51 @@
 import os
 from pathlib import Path
 
+# import db_updater
 from dotenv import load_dotenv
 from pyinaturalist import get_observations
+from pyinaturalist_convert import to_dataframe
+
+# import format_module
 
 # Loads .env variables
 load_dotenv()
 
-# To be sure to be placed as repo directory level
+# To obtain actual path to inat_fetcher dir
 p = Path(__file__).parents[1]
-print(p)
-os.chdir(p)
 
 # Set up paths and filenames
 data_out_path = "/data/out/"
 output_filename = "inat_observations"
 filename_suffix = "csv"
-path_to_output_file = os.path.join(data_out_path, output_filename + "." + filename_suffix)
+path_to_output_file = os.path.join(str(p) + data_out_path, output_filename + "." + filename_suffix)
 
-# Import env variable
+# import env variable
 access_token = os.getenv("ACCESS_TOKEN")
 
-response = get_observations(
-    project_id=130644,
-    page="all",
-    per_page=300,
-    # access_token=access_token
-)
-print(response)
+response = get_observations(project_id=130644, page="all", per_page=200, access_token=access_token)
+df = to_dataframe(response)
+
+# Before exporting we move the id column to the beginning since it is needed to be at this position to be detected as a PK in airtbale or siomnilar dbs
+
+# shift column 'id' to first position
+first_column = df.pop("id")
+
+# insert column using insert(position,column_name,
+# first_column) function
+df.insert(0, "id", first_column)
+
+# formatting of data
+# format_module.location_formatting(df,'location','swiped_loc')
+# format_module.dbgi_id_extract(df)
+
+# We keep the table
+df.to_csv(path_to_output_file, index=False)
+
+
+# update the database using db_updater.py script
+if os.path.exists(path_to_output_file):
+    print("path: ${path_to_output_file}")
+    # db_updater
+else:
+    print("csv generation error")
