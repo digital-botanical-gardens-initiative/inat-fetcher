dca5965629720a49749d2aa55465e9c5d0422bb6
edouardbruelhart <edouard.bruelhart@unifr.ch>
2024-03-22 10:07:20 +0100

    corrected tests and checks errors

--- tests/test_foo.py
+++ tests/test_foo.py
@@ -1,11 +1,5 @@
-
-import sys
-import os
-
-# Add directory containing the inat_fetcher package to the Python path
-inat_fetcher_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-sys.path.append(inat_fetcher_path)
 from inat_fetcher.src.foo import foo
+
 
 def test_foo():
     assert foo() == "foo"
