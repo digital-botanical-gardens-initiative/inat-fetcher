a374d7e6600c7652bbb1cf4a935781cc55d27119
edouardbruelhart <edouard.bruelhart@unifr.ch>
2024-04-03 16:38:47 +0200

    update some stuff

--- inat_fetcher/src/db_updater.py
+++ inat_fetcher/src/db_updater.py
@@ -1,1 +1,172 @@
-print("correctly launched dbupdater")
+
+import requests
+import json
+
+# Define the Directus API endpoint and authentication token
+DIRECTUS_URL = "https://your-directus-instance.com/api/1.1"
+AUTH_TOKEN = "your-auth-token"
+
+# Define the collection name
+COLLECTION_NAME = "your_collection_name"
+
+# Define the fields to create
+fields = [
+    {"field": "id", "type": "numeric"},
+    {"field": "quality_grade", "type": "string", "length": 25},
+    {"field": "time_observed_at", "type": "datetime"},
+    {"field": "taxon_geoprivacy", "type": "string", "length": 25},
+    {"field": "annotations", "type": "string", "length": 25},
+    {"field": "uuid", "type": "text"},
+    {"field": "cached_votes_total", "type": "numeric"},
+    {"field": "identifications_most_agree", "type": "boolean"},
+    {"field": "species_guess", "type": "string", "length": 100},
+    {"field": "identifications_most_disagree", "type": "boolean"},
+    {"field": "tags", "type": "string", "length": 150},
+    {"field": "positional_accuracy", "type": "numeric"},
+    {"field": "comments_count", "type": "numeric"},
+    {"field": "site_id", "type": "boolean"},
+    {"field": "license_code", "type": "string", "length": 25},
+    {"field": "quality_metrics", "type": "text"},
+    {"field": "public_positional_accuracy", "type": "numeric"},
+    {"field": "reviewed_by", "type": "string", "length": 60},
+    {"field": "oauth_application_id", "type": "numeric"},
+    {"field": "flags", "type": "string", "length": 25},
+    {"field": "created_at", "type": "datetime"},
+    {"field": "description", "type": "text"},
+    {"field": "project_ids_with_curator_id", "type": "string", "length": 25},
+    {"field": "updated_at", "type": "datetime"},
+    {"field": "sounds", "type": "string", "length": 25},
+    {"field": "place_ids", "type": "string", "length": 250},
+    {"field": "captive", "type": "boolean"},
+    {"field": "ident_taxon_ids", "type": "text"},
+    {"field": "outlinks", "type": "text"},
+    {"field": "faves_count", "type": "numeric"},
+    {"field": "ofvs", "type": "text"},
+    {"field": "num_identification_agreements", "type": "numeric"},
+    {"field": "comments", "type": "text"},
+    {"field": "map_scale", "type": "numeric"},
+    {"field": "uri", "type": "string", "length": 250},
+    {"field": "project_ids", "type": "string", "length": 25},
+    {"field": "community_taxon_id", "type": "numeric"},
+    {"field": "owners_identification_from_vision", "type": "boolean"},
+    {"field": "identifications_count", "type": "numeric"},
+    {"field": "obscured", "type": "boolean"},
+    {"field": "num_identification_disagreements", "type": "numeric"},
+    {"field": "geoprivacy", "type": "boolean"},
+    {"field": "location", "type": "string", "length": 100},
+    {"field": "votes", "type": "text"},
+    {"field": "spam", "type": "boolean"},
+    {"field": "mappable", "type": "boolean"},
+    {"field": "identifications_some_agree", "type": "boolean"},
+    {"field": "project_ids_without_curator_id", "type": "string", "length": 25},
+    {"field": "place_guess", "type": "text"},
+    {"field": "identifications", "type": "text"},
+    {"field": "project_observations", "type": "text"},
+    {"field": "photos", "type": "text"},
+    {"field": "faves", "type": "text"},
+    {"field": "non_owner_ids", "type": "text"},
+    {"field": "observed_on", "type": "datetime"},
+    {"field": "photo_url", "type": "text"},
+    {"field": "taxon_is_active", "type": "boolean"},
+    {"field": "taxon_ancestry", "type": "text"},
+    {"field": "taxon_min_species_ancestry", "type": "text"},
+    {"field": "taxon_endemic", "type": "boolean"},
+    {"field": "taxon_iconic_taxon_id", "type": "numeric"},
+    {"field": "taxon_min_species_taxon_id", "type": "numeric"},
+    {"field": "taxon_threatened", "type": "boolean"},
+    {"field": "taxon_rank_level", "type": "numeric"},
+    {"field": "taxon_introduced", "type": "boolean"},
+    {"field": "taxon_native", "type": "boolean"},
+    {"field": "taxon_parent_id", "type": "numeric"},
+    {"field": "taxon_name", "type": "string", "length": 100},
+    {"field": "taxon_rank", "type": "string", "length": 25},
+    {"field": "taxon_extinct", "type": "boolean"},
+    {"field": "taxon_id", "type": "numeric"},
+    {"field": "taxon_ancestor_ids", "type": "text"},
+    {"field": "taxon_photos_locked", "type": "boolean"},
+    {"field": "taxon_taxon_schemes_count", "type": "numeric"},
+    {"field": "taxon_wikipedia_url", "type": "text"},
+    {"field": "taxon_current_synonymous_taxon_ids", "type": "text"},
+    {"field": "taxon_created_at", "type": "datetime"},
+    {"field": "taxon_taxon_changes_count", "type": "numeric"},
+    {"field": "taxon_complete_species_count", "type": "boolean"},
+    {"field": "taxon_universal_search_rank", "type": "numeric"},
+    {"field": "taxon_observations_count", "type": "numeric"},
+    {"field": "taxon_flag_counts_resolved", "type": "numeric"},
+    {"field": "taxon_flag_counts_unresolved", "type": "numeric"},
+    {"field": "taxon_atlas_id", "type": "string", "length": 50},
+    {"field": "taxon_default_photo_id", "type": "numeric"},
+    {"field": "taxon_default_photo_license_code", "type": "string", "length": 25},
+    {"field": "taxon_default_photo_attribution", "type": "text"},
+    {"field": "taxon_default_photo_url", "type": "text"},
+    {"field": "taxon_default_photo_original_dimensions_height", "type": "numeric"},
+    {"field": "taxon_default_photo_original_dimensions_width", "type": "numeric"},
+    {"field": "taxon_default_photo_flags", "type": "text"},
+    {"field": "taxon_default_photo_square_url", "type": "text"},
+    {"field": "taxon_default_photo_medium_url", "type": "text"},
+    {"field": "taxon_iconic_taxon_name", "type": "string", "length": 25},
+    {"field": "taxon_preferred_common_name", "type": "string", "length": 100},
+    {"field": "preferences_prefers_community_taxon", "type": "boolean"},
+    {"field": "geojson_coordinates", "type": "string", "length": 100},
+    {"field": "geojson_type", "type": "string", "length": 25},
+    {"field": "user_site_id", "type": "numeric"},
+    {"field": "user_created_at", "type": "datetime"},
+    {"field": "user_id", "type": "numeric"},
+    {"field": "user_login", "type": "string", "length": 25},
+    {"field": "user_spam", "type": "boolean"},
+    {"field": "user_suspended", "type": "boolean"},
+    {"field": "user_login_autocomplete", "type": "string", "length": 25},
+    {"field": "user_login_exact", "type": "string", "length": 25},
+    {"field": "user_name", "type": "string", "length": 25},
+    {"field": "user_name_autocomplete", "type": "string", "length": 25},
+    {"field": "user_orcid", "type": "boolean"},
+    {"field": "user_icon", "type": "text"},
+    {"field": "user_observations_count", "type": "numeric"},
+    {"field": "user_identifications_count", "type": "numeric"},
+    {"field": "user_journal_posts_count", "type": "numeric"},
+    {"field": "user_activity_count", "type": "numeric"},
+    {"field": "user_species_count", "type": "numeric"},
+    {"field": "user_universal_search_rank", "type": "numeric"},
+    {"field": "user_roles", "type": "string", "length": 25},
+    {"field": "user_icon_url", "type": "text"},
+    {"field": "taxon_default_photo", "type": "boolean"},
+    {"field": "taxon_conservation_status_place_id", "type": "boolean"},
+    {"field": "taxon_conservation_status_source_id", "type": "numeric"},
+    {"field": "taxon_conservation_status_user_id", "type": "boolean"},
+    {"field": "taxon_conservation_status_authority", "type": "string", "length": 250},
+    {"field": "taxon_conservation_status_status", "type": "string", "length": 25},
+    {"field": "taxon_conservation_status_status_name", "type": "string", "length": 50},
+    {"field": "taxon_conservation_status_geoprivacy", "type": "string", "length": 50},
+    {"field": "taxon_conservation_status_iucn", "type": "string", "length": 25},
+    {"field": "observed_on_details", "type": "boolean"},
+    {"field": "created_time_zone", "type": "string", "length": 100},
+    {"field": "observed_time_zone", "type": "string", "length": 100},
+    {"field": "time_zone_offset", "type": "string", "length": 100},
+    {"field": "observed_on_string", "type": "boolean"},
+    {"field": "created_at_details_date", "type": "datetime"},
+    {"field": "created_at_details_week", "type": "numeric"},
+    {"field": "created_at_details_month", "type": "numeric"},
+    {"field": "created_at_details_hour", "type": "numeric"},
+    {"field": "created_at_details_year", "type": "numeric"},
+    {"field": "created_at_details_day", "type": "numeric"},
+    {"field": "swiped_loc", "type": "string", "length": 100},
+    {"field": "emi_external_id", "type": "string", "length": 50},
+]
+
+# Construct the request payload
+payload = {"fields": fields}
+
+# Construct headers with authentication token
+headers = {"Authorization": f"Bearer {AUTH_TOKEN}", "Content-Type": "application/json"}
+
+# Construct the API endpoint for creating fields
+endpoint = f"{DIRECTUS_URL}/tables/{COLLECTION_NAME}/fields"
+
+# Send the POST request to create the fields
+response = requests.post(endpoint, headers=headers, data=json.dumps(payload))
+
+# Check if the request was successful
+if response.status_code == 200:
+    print("Fields created successfully.")
+else:
+    print(f"Error: {response.status_code} - {response.text}")
--- inat_fetcher/src/emi_id_extracter.py
+++ inat_fetcher/src/emi_id_extracter.py
@@ -2,6 +2,7 @@ import os
 from pathlib import Path
 
 import pandas as pd
+import re
 
 # To obtain actual path to inat_fetcher dir
 p = Path(__file__).parents[1]
@@ -11,9 +12,12 @@ data_in_path = "/data/in/"
 data_out_path = "/data/out/"
 input_filename = "inat_observations_loc"
 output_filename = "inat_observations_treated"
-filename_suffix = "csv"
-path_to_input_file = os.path.join(str(p) + data_in_path, input_filename + "." + filename_suffix)
-path_to_output_file = os.path.join(str(p) + data_out_path, output_filename + "." + filename_suffix)
+recovery_filename = "inat_observation_recovery"
+filename_suffix = ".csv"
+path_to_input_file = os.path.join(str(p) + data_in_path, input_filename + filename_suffix)
+path_to_output_file = os.path.join(str(p) + data_out_path, output_filename + filename_suffix)
+path_to_recovery_file = os.path.join(str(p) + data_out_path, recovery_filename + filename_suffix)
+pattern = re.compile(r"dbgi_\d{6}")
 
 # Load dataframe
 df = pd.read_csv(path_to_input_file)
@@ -23,12 +27,19 @@ df["emi_external_id"] = "NA"
 
 # loop over each line
 for i in range(len(df)):
-    # if the ofvs line is not empty
+    # if the 'tags' line is not empty
     if df.tags[i] != "[]":
-        cleaned_string = df.tags[i][2:-2]
-        print(cleaned_string)
+        cleaned_string = str.replace(df.tags[i][2:-2], "emi_external_id:", "")
+        df.loc[i, "emi_external_id"] = cleaned_string
+    elif df["ofvs.15466"][i] != "":
+        df.loc[i, "emi_external_id"] = str(df['ofvs.15466'][i])
 
-# We keep the table
-df.to_csv(path_to_output_file, index=False)
+# Split dataframe based on emi_external_id column matching the pattern
+pattern_matched_df = df[df["emi_external_id"].str.match(pattern)]
+pattern_unmatched_df = df[~df["emi_external_id"].str.match(pattern)]
 
-print("csv correctly updated")
+# We keep the tables
+pattern_matched_df.to_csv(path_to_output_file, index=False)
+pattern_unmatched_df.to_csv(path_to_recovery_file, index=False)
+
+print("csvs correctly updated")
