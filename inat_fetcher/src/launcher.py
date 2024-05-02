13ddf1fdf9cad9e006b669e4f927ef734a26fdca
edouardbruelhart <edouard.bruelhart@unifr.ch>
2024-05-02 14:05:08 +0200

    renamed python to python3

--- inat_fetcher/src/launcher.py
+++ inat_fetcher/src/launcher.py
@@ -9,19 +9,19 @@ scripts_folder = "/src/"
 path_to_scripts = os.path.join(str(p) + scripts_folder)
 
 # Run fetcher
-subprocess.run(["python", f"{path_to_scripts}fetcher.py"]).check_returncode()
+subprocess.run(["python3", f"{path_to_scripts}fetcher.py"]).check_returncode()
 
 # Run location formatter
-subprocess.run(["python", f"{path_to_scripts}location_formatter.py"]).check_returncode()
+subprocess.run(["python3", f"{path_to_scripts}location_formatter.py"]).check_returncode()
 
 # Run emi id extracter
-subprocess.run(["python", f"{path_to_scripts}emi_id_extracter.py"]).check_returncode()
+subprocess.run(["python3", f"{path_to_scripts}emi_id_extracter.py"]).check_returncode()
 
 # Run create directus fields
-subprocess.run(["python", f"{path_to_scripts}create_directus_fields.py"]).check_returncode()
+subprocess.run(["python3", f"{path_to_scripts}create_directus_fields.py"]).check_returncode()
 
 # Run db updater
-subprocess.run(["python", f"{path_to_scripts}db_updater.py"]).check_returncode()
+subprocess.run(["python3", f"{path_to_scripts}db_updater.py"]).check_returncode()
 
 # Run directus link maker
-subprocess.run(["python", f"{path_to_scripts}directus_link_maker.py"]).check_returncode()
+subprocess.run(["python3", f"{path_to_scripts}directus_link_maker.py"]).check_returncode()
